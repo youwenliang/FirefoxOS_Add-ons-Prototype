2f60ce8977346507acd6889d0c74d69715336afa
Mark Liang <big.head.cool@gmail.com>
2015-10-16 18:28:39 +0800

    Update toggle example, trying to make it draggable & add home function.

--- js/index.js
+++ js/index.js
@@ -34,27 +34,3 @@ function initialize() {
     }
   }
 }
-
-window.onload = addListeners();
-
-function addListeners(){
-    document.getElementById('dxy').addEventListener('mousedown', mouseDown, false);
-    window.addEventListener('mouseup', mouseUp, false);
-
-}
-
-function mouseUp()
-{
-    window.removeEventListener('mousemove', divMove, true);
-}
-
-function mouseDown(e){
-  window.addEventListener('mousemove', divMove, true);
-}
-
-function divMove(e){
-    var div = document.getElementById('dxy');
-  div.style.position = 'absolute';
-  div.style.top = e.clientY-30 + 'px';
-  div.style.left = e.clientX-30 + 'px';
-}
