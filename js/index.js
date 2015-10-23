d39176db027e4b3fd4c78c6f65ee974346f897bd
Mark Liang <big.head.cool@gmail.com>
2015-10-23 16:56:51 +0800

    Refactor Stable version

--- js/index.js
+++ js/index.js
@@ -1,13 +1,5 @@
-(function() {
-    function FloatButton(stage){
-    	this.stage = stage;
-        this.button = document.createElement("div");
-        this.container = document.createElement("div");
-        this.background = document.createElement("div");
-        this.description = document.createElement("p");
-        this.init();
-    }
-    
+(function() {    
+    //Global Variables
     var CLICK_INTERVAL = 250;
   	var HOLD_INTERVAL = 350;
 	  var MOVE_THRESHOLD = 5;
@@ -22,6 +14,16 @@
     };
     
     var stage = 0;
+
+    //Float Button
+    function FloatButton(stage){
+        this.stage = stage;
+        this.button = document.createElement("div");
+        this.container = document.createElement("div");
+        this.background = document.createElement("div");
+        this.description = document.createElement("p");
+        this.init();
+    }
     
     FloatButton.prototype = {
         container: null,
@@ -40,9 +42,8 @@
             var background = this.background;
             var description = this.description;
             
-            
           	button.id = "button";
-      		button.className = "circle";
+        		button.className = "circle";
             background.id = "background";
             background.className = "circle background";
             container.className = "container origin";
@@ -69,7 +70,7 @@
            	container.appendChild(description);
         },
         registerEvents: function(){
-        	var button = this.button;
+            var button = this.button;
             var background = this.background;
             var timerID;
             var touchStartTimeStamp;
@@ -85,12 +86,6 @@
                 evt.target.className += " hovering";
                 this.container.classList.add('move');
                 button.innerHTML = '<img src="' + ICONS["close"] + '">';
-                // var left = (screen.width) - 9;
-                // var style = `
-                //   left: ${left}px;
-                // `;
-                // container.setAttribute('style', style);
-
                 
                 timerID = setTimeout(() => {
                   if (! moved) {
@@ -105,41 +100,43 @@
             //Touch End
             button.addEventListener('touchend', (evt) => {
                 var time = new Date() - touchStartTimeStamp;
+                
                 this.container.classList.remove('move');
-				          evt.target.className = "circle";
+				        evt.target.className = "circle";
                 background.className = "circle background";
+                
                 this.menu.close();
                 stage = 0;
                 button.innerHTML = '<img src="' + ICONS["menu"] + '">';
+                
                 clearTimeout(timerID);
                 timerID = -1;
             });
             
              //Touch Move
-             var body = document.querySelector('body');
-	 		 body.addEventListener('touchmove', (evt) => {
-                 var xpos = evt.touches[0].clientX;
-                 var ypos = evt.touches[0].clientY;
-                 var dist = Math.sqrt(Math.pow(xpos-321,2)+Math.pow(ypos-330,2));
+            var body = document.querySelector('body');
+	 		          body.addEventListener('touchmove', (evt) => {
+                var xpos = evt.touches[0].clientX;
+                var ypos = evt.touches[0].clientY;
+                var dist = Math.sqrt(Math.pow(xpos-321,2)+Math.pow(ypos-330,2));
                  
-                 if(dist < 39 && !mouseover) {
-                     mouseover = true;
-                     console.log("mouseover");
-                     if(stage == 1){
-                     	button.classList.add('hovering');
-                 	}
-                 }
-                 else if(dist > 39 && mouseover) {
-                     mouseover = false;
-                     console.log("mouseout");
-                     if(stage == 1){
-                     	button.classList.remove('hovering');
-                 	}
-                 }
-             });            
-
+                if(dist < 39 && !mouseover) {
+                    mouseover = true;
+                    if(stage == 1){
+                        button.classList.add('hovering');
+                 	  }
+                }
+                else if(dist > 39 && mouseover) {
+                    mouseover = false;
+                    if(stage == 1){
+                        button.classList.remove('hovering');
+                 	  }
+                }
+            });            
         }
     }
+
+    //Menu Options
     function MenuOptions(container){
     	this.container = container;
         this.items = [];
@@ -151,9 +148,9 @@
     }
     
     Object.defineProperty(MenuOptions.prototype, 'opened', {
-	    get: function() {
+        get: function() {
       		return this.container.classList.contains('opened');
-    	}
+        }
   	});
     
     MenuOptions.prototype = {
@@ -167,31 +164,33 @@
         },
         removeItem: function(id) {
             for (var i = 0; i < this.items.length; i++) {
-              if (this.items[i].id === id) {
-                this.items.splice(i, 1);
-                break;
-              }
+                if (this.items[i].id === id) {
+                  this.items.splice(i, 1);
+                  break;
+                }
             }
         },
         render: function(){
             this.renderedCount = this.items.length;
-    		this.items.forEach(this.renderItem.bind(this));
+    		    this.items.forEach(this.renderItem.bind(this));
             this.items.forEach(this.registerEvents.bind(this));
-		},
+		    },
         renderItem: function(item) {
             if(this.renderedCount<3){
-              this.startAngle = 180/(this.renderedCount+1)-90;
-              this.addAngle = 180/(this.renderedCount+1);
-            } else {
-              this.startAngle = -90;
-              this.addAngle = 180/(this.renderedCount-1);
+                this.startAngle = 180/(this.renderedCount+1)-90;
+                this.addAngle = 180/(this.renderedCount+1);
+            } 
+            else {
+                this.startAngle = -90;
+                this.addAngle = 180/(this.renderedCount-1);
             }
+
             var menu = document.createElement('div');	
             menu.role = 'menu';
             menu.classList.add('circle-item');
             menu.classList.add('origin');
             item.classLists.forEach((cls) => {
-              menu.classList.add(cls);
+                menu.classList.add(cls);
             });
             menu.id = item.id;
             menu.innerHTML = '<img src="' + ICONS[item.id] + '">';
@@ -205,69 +204,75 @@
             this.renderIndex++;
         },
         open: function(){
-            console.log("open");
             setTimeout(() => {
             this.container.classList.remove('origin');
                 setTimeout(() => {
-  	            this.container.classList.add('open');
-	          }, 350);
-          }, 50);
+                    this.container.classList.add('open');
+                }, 350);
+            }, 50);
         },
         close: function(){
-            console.log("close");
             setTimeout(() => {
   	            this.container.classList.remove('open');
-	        }, 350);
+            }, 350);
             this.container.classList.add('origin');
         },
         registerEvents: function(item){
+            var mouseover = false;
+            var currentId = -1;
+            var body = document.querySelector('body');
+
             //Touch Move
-             var mouseover = false;
-             var currentId = -1;
-             var body = document.querySelector('body');
-	 		 body.addEventListener('touchmove', (evt) => {
-                 var xpos = evt.touches[0].clientX;
-                 var ypos = evt.touches[0].clientY;
-                 var _x = parseInt(this._xpos[item.index])+321;
-                 var _y = parseInt(this._ypos[item.index])+330;
-                 var dist = Math.sqrt(Math.pow(xpos-_x,2)+Math.pow(ypos-_y,2));
+	 		      body.addEventListener('touchmove', (evt) => {
+                var xpos = evt.touches[0].clientX;
+                var ypos = evt.touches[0].clientY;
+                var _x = parseInt(this._xpos[item.index])+321;
+                var _y = parseInt(this._ypos[item.index])+330;
+                var dist = Math.sqrt(Math.pow(xpos-_x,2)+Math.pow(ypos-_y,2));
                  
-                 if(dist < 39 && !mouseover) {
-                     console.log("mouseover");
-                     if(stage == 1){
-                                              mouseover = true;
+                if(dist < 39 && !mouseover) {
+                    if(stage == 1){
+                        mouseover = true;
                         document.getElementById("description").textContent = item.id;
-                     	document.getElementById(item.id).className +=' hovering';
+                       	document.getElementById(item.id).className +=' hovering';
                         document.getElementById(item.id).style.transform += " scale3d(1.3,1.3,1.3)";
                         currentId = item.id;
-                 	}
-                 }
-                 else if(dist > 39 && mouseover) {
-
-                     console.log("mouseout");
-                     if(stage == 1){
-                                              mouseover = false;
-                         document.getElementById("description").textContent = "";
-                     	document.getElementById(item.id).className = document.getElementById(item.id).className.split('hover')[0];
+                 	  }
+                }
+                else if(dist > 39 && mouseover) {
+                    if(stage == 1){
+                        mouseover = false;
+                        document.getElementById("description").textContent = "";
+                  	    document.getElementById(item.id).className = document.getElementById(item.id).className.split('hover')[0];
                         document.getElementById(item.id).style.transform = document.getElementById(item.id).style.transform.split('scale')[0];
                         currentId = -1;
-                 	}
-                 }
+               	    }
+                }
              });
-             body.addEventListener('touchend', (evt) => {
-                 if(mouseover){
-                     console.log(currentId);
-                     navigator.vibrate(50);
-                     window.dispatchEvent(new CustomEvent(currentId));
-                     setTimeout(function(){
-                         document.getElementById("description").textContent = "";
-                         document.getElementById(currentId).className = document.getElementById(currentId).className.split('hover')[0];
-                         document.getElementById(currentId).style.transform = document.getElementById(currentId).style.transform.split('scale')[0];
-                         currentId = -1;
-                         mouseover = false;
-                     },100);
-                 }
-             });
+
+            //Touch End
+            body.addEventListener('touchend', (evt) => {
+                document.getElementById("description").textContent = "";
+                if(mouseover){
+                    console.log(currentId);
+                    navigator.vibrate(50);
+
+                    //Implement Functions
+                    if(currentId == "back"){
+                        window.dispatchEvent(new CustomEvent("holdhome")); 
+                    }
+                    else {
+                        window.dispatchEvent(new CustomEvent(currentId));
+                    }
+
+                    setTimeout(function(){
+                        document.getElementById(currentId).className = document.getElementById(currentId).className.split('hover')[0];
+                        document.getElementById(currentId).style.transform = document.getElementById(currentId).style.transform.split('scale')[0];
+                        currentId = -1;
+                        mouseover = false;
+                    },100);
+                }
+            });
         }
     }
     
@@ -276,11 +281,12 @@
     var floatButton;
       //Initialize HTML
     if (document.readyState !== 'loading') {
-      init();
-    } else {
-      document.addEventListener('readystatechange', function() {
-        document.readyState === 'interactive' && init();
-      });
+        init();
+    } 
+    else {
+        document.addEventListener('readystatechange', function() {
+            document.readyState === 'interactive' && init();
+        });
     }
 
     function init() {
